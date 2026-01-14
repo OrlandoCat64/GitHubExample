f1d4a53d00e63c9fbc629d959bddbfacaed0dc89
FFlores <francoj00@hotmail.com>
2026-01-14 15:08:31 -0600

    La resta sirve de manera correcta y funcional

--- PL/Program.cs
+++ PL/Program.cs
@@ -11,26 +11,18 @@ namespace PL
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Calamardo es el mejor amiko en el mundo ");
-            Console.WriteLine("Si lees esto es el branch01");
-
-            int macaco = 23;
-            int chango = 12;
-            int resultado = macaco + chango;
-            Console.WriteLine(resultado);
-
-
+          
             int result1 = 0;
             int result2 = 0;
 
-            Console.WriteLine("Bienvenido a la calculadora que solo suma");
+            Console.WriteLine("Bienvenido a la calculadora que solo resta");
             Console.WriteLine("Ingresa el primer número");
             result1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Ingresa el segundo número");
             result2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Resultado es igual a " + (result1 + result2));
+            Console.WriteLine("Resultado es igual a " + (result1 - result2));
         }
     }
 }
